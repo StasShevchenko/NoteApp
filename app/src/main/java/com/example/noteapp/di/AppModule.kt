dc48c24bd4897598ca31f2e409e95b274a6010ad
Stas <dark.knight22832@gmail.com>
2023-03-05 11:29:32 +0300

    DI is configured.

---
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.noteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.noteapp.data.data_source.NotesDao
+import com.example.noteapp.data.data_source.NotesDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNotesDatabase(app: Application): NotesDatabase{
+        return Room.databaseBuilder(
+            app,
+            NotesDatabase::class.java,
+            NotesDatabase.DATABASE_NAME
+        )
+            .build()
+    }
+
+    @Provides
+    fun provideNotesDao(db: NotesDatabase): NotesDao {
+        return db.notesDao
+    }
+}
